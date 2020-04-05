1adf4f413cd828a744983e2b3f195d90c3006964
Mikael Austin <mikaelaustin@Mikaels-MBP.verizon.net>
2020-04-05 17:39:44 -0400

    firestore connection

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,17 +1,40 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as firebaseHelper from 'firebase-functions-helper/dist';
 import * as express from 'express'
+
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+const contactsCollection = "DistributionMembers"
 
 const app = express();
 
-// define a route handler for the default home page
-app.get( "/signup", ( req, res ) => {
-    // render the index template
-    console.log(req)
-    res.send("test")
-    //res.render( "/contact/thanks" );
-} );
+interface Contact {
+    Name: String
+    Email: String
+}
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+// define a route handler for the default home page
+app.post( "/signup", async ( req, res ) => {
+    // render the index template
+    console.log(req.body.name);
+    console.log(req.body.email);
+    try {
+
+        const contact: Contact = {
+            Name: req.body.name,
+            Email: req.body.email
+        }
+
+        const newContact = await firebaseHelper.firestore
+            .createNewDocument(db, contactsCollection, contact);
+        res.status(201).send(`Created a new contact: ${newContact.id}`);
+    } catch (error) {
+        console.log("Error: ", error)
+        res.status(400).send(`Contact should only contains firstName, lastName and email!!!`);
+    }  
+
+    //res.redirect("/contact/thanks");
+});
+
 export const pp = functions.https.onRequest(app);
